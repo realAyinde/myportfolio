8b44cdfd1c3b95a3dee2ce67bad74a53d0f513e7
Ayinde Sodiq <58987770+realAyinde@users.noreply.github.com>
2021-11-01 08:18:36 +0100

    make changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles/app.scss";
+
+let others = document.getElementById("others")
+
+var otherLists = [];
+
+// otherLists.push(others.getElementById(interests))
+// otherLists.push(others.getElementById(education))
+// otherLists.push(others.getElementById(work_experience))
+// otherLists.push(others.getElementById(skills))
+// otherLists.push(others.getElementById(languages))
+// otherLists.push(others.getElementById(resume))
+
+console.log(
+  others 
+);
